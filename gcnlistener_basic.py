b56721a002c30f4a384af74151d8774b5d0a9d5c
Lanasalmon <lana.salmon@ucdconnect.ie>
2019-10-19 12:10:25 +0100

    Top level functions.

--- gcnlistener_basic.py
+++ gcnlistener_basic.py
@@ -1,44 +1,23 @@
-
 import sys
-import os
-import json
-import datetime
 import lxml.etree
 import gcn
-import lxml.etree
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-import boto3, botocore
-from email.mime.base import MIMEBase
-from email import encoders
-import smtplib
 import healpy as hp
 import numpy as np
-import ligo.skymap.plot
 import astropy_healpix
-from astropy.io import fits
 import astropy.utils.data
 from astropy.coordinates import SkyCoord
 import astropy.units as u
-from astroquery.simbad import Simbad
-from astroquery.vizier import VizierClass
-from astropy.coordinates import SkyCoord
-import astropy.units as u
-import numpy.ma as ma
-import time
-import matplotlib
 import pandas as pd
-import psycopg2
-import operator
-from numpy.linalg import eig, inv
-import functools
-from ligo.skymap.io import read_sky_map
 from ligo.skymap.postprocess import crossmatch
-import h5py
-from db import * 
-from probability import *
-from dataframe import * 
-from contours import *
+from ligo.skymap.io import read_sky_map
+import ligo.skymap.plot
+from probability import calculate_absolute_probability,unique_galaxies,extract_LIGO_probability,makelists,sortbyprob
+from dataframe import create_dataframe
+from contours import contour_plots,join_0_360,split_contours,integrated_probability,hpix_contours
+from createfiles import createtxt,createjsonfile,createasciifile
+from GLADE import GLADEV2coordinates
+from skymapio import readskymap
+
 
 sys.setrecursionlimit(6000000)
 # Function to call every time a GCN is received.
@@ -48,123 +27,6 @@ sys.setrecursionlimit(6000000)
     gcn.notice_types.LVC_PRELIMINARY,
     gcn.notice_types.LVC_INITIAL,
     gcn.notice_types.LVC_UPDATE)
-
-def createtxtfile(dataf):
-    c=SkyCoord(ra=dataf['RA (degrees)'].values*u.degree, dec=dataf['Dec (degrees)'].values*u.degree)
-
-    newrah=[c.ra.hms.h]
-    newram=[c.ra.hms.m]
-    newras=[c.ra.hms.s]
-    newdecs=[c.dec]
-
-    newc=[str(int(newrah[0][c]))+ ' ' +str(int(newram[0][c]))+' '+str(newras[0][c].round(2))+ ' '+str(newdecs[0][c]) for c,x in enumerate(newrah)]
-
-    newc = [w.replace('d', ' ') for w in newc]
-    newc = [w.replace('h', ' ') for w in newc]
-    newc = [w.replace('m', ' ') for w in newc]
-    newc = [w.replace('s', ' ') for w in newc]
-    ccc.append(newc)
-
-    for i in range(0,len(ccc)):
-        coords=[]
-        name=[]
-        jtwo=[]
-        probtxt=[]
-        galname=[]
-        exclam=[]
-        for k in range(0,len(ccc[i])):
-
-            coords.append(ccc[i][k])
-            jtwo.append('J2000')
-            name.append('COORD0'+str(k))
-            exclam.append('!')
-            galname.append(str(finalgalnamelist[finaldictsorted[k][0]]))
-            probtxt.append(str(finaldictsorted[k][1]))
-        datafc=pd.DataFrame(
-                     {'Galaxy Name' : name,
-                     'Coordinates': coords,
-                     'J2000': jtwo,
-                     '!': exclam,
-                     'Name' :galname,
-                     'Prob': probtxt
-                     })
-
-        datafc = datafc[['Galaxy Name', 'Coordinates', 'J2000','!','Name','Prob']]
-        tfile = open(graceid+prelim+str(levelsper[d])+".txt", 'a')
-        tfile.write(datafc.to_string(header=False, index=False))
-        tfile.close()
-    return   
-def GLADEV2coordinates():
-
-    vizier = VizierClass(
-    row_limit=-1, columns=['HyperLEDA', '_RAJ2000', '_DEJ2000', 'Dist','Bmag'],column_filters={'Bmag':'!=null'})
-    cat, = vizier.get_catalogs('VII/281/glade2')
-    data=pd.DataFrame({'RA': cat['_RAJ2000'], 'Dec':cat['_DEJ2000'],'dist':cat['Dist'],'Bmag':cat['Bmag'],'HyperLEDA':cat['HyperLEDA']})
-    msk1=data[['dist']]<=distmax
-    msk2=data[['dist']]>=distmin
-    msk3=data[['dist']]>0
-    msk4=data[['dist']]!='NaN'
-    msk5=data[['Bmag']]!='NaN' 
-    msk6=data[['Bmag']]!='null' 
-    msk7=data[['Bmag']]>0 
-    msk=pd.concat((msk1,msk2,msk3,msk4,msk5,msk6,msk7),axis=1)
-    slct=msk.all(axis=1)
-    data=data.ix[slct]
-
-    coordinates=SkyCoord(data['RA'].values*u.deg, data['Dec'].values*u.deg,data['dist'].values*u.Mpc)
-    return coordinates
-def readskymap(skymap):
-        
-    # Read the HEALPix sky map and the FITS header.
-
-    hdulist = fits.open(skymap)
-    distest = hdulist[1].header['DISTMEAN']
-    diststd= hdulist[1].header['DISTSTD']
-
-
-    skymap=read_sky_map(skymap, moc=False,distances=True)
-
-    prob=skymap[0][0]
-    distmu=skymap[0][1]
-    distsigma=skymap[0][2]
-    distnorm=skymap[0][3]
-    npix = len(prob)
-    nside=hp.npix2nside(npix)
-    return skymap, prob, distmu, distsigma, distnorm, npix, nside, distest, diststd
-
-def createjsonfile(jsonlist):
-    csv=str('[')
-    for i in range(0,len(jsonlist)):
-        if i<len(jsonlist)-1:
-            csv= csv+ str(jsonlist[i])+','
-        else:
-            csv = csv + str(jsonlist[i])
-    csv= csv+ ']'
-    f = open(graceid+prelim+str(levelsper[d])+".json", "w")
-    f.write(csv)      # str() converts to string
-    f.close()
-    return
-
-
-def createasciifile(jsonlist2):
-
-    csv2=str('')
-    for i in range(0,len(jsonlist2)):
-
-        csv2= csv2+ str(jsonlist2[i])
-    f = open(graceid+prelim+str(levelsper[d])+".dat", "w")
-    f.write( csv2 )      # str() converts to string
-    f.close()
-    return
-
-def sortbyprob(finalprobslist):
-
-    Snumber=np.arange(0,len(finalprobslist),1)
-    fullprobs = dict(zip(Snumber, finalprobslist))
-    finaldictsorted = sorted(fullprobs.items(), key=operator.itemgetter(1), reverse=True)
-    finalprobsorted=sorted(finalprobslist, reverse=True)
-    cumsumprobs=np.cumsum(finalprobsorted)
-    return finaldictsorted, cumsumprobs
 
 def process_gcn(payload, root):
     
@@ -203,7 +65,7 @@ def process_gcn(payload, root):
     distmin=distest-3*diststd
     
     #get coordinates of all GLADEV2 galaxies
-    coordinates=GLADEV2coordinates()
+    coordinates=GLADEV2coordinates(distmax,distmin)
     
     #crossmatch GLADE with multiorder skymap
     url='https://gracedb.ligo.org/api/superevents/'+graceid+'/files/bayestar.multiorder.fits'
@@ -285,10 +147,9 @@ def process_gcn(payload, root):
         #create files
         jsonlist.append(dataf[['Galaxy name', 'Galaxy probability', 'RA (degrees)', 'Dec (degrees)','Distance (Mpc)', 'B magnitude', 'Cumulative Probability']].to_json())
         jsonlist2.append(dataf[['Galaxy name', 'Galaxy probability', 'RA (degrees)', 'Dec (degrees)','Distance (Mpc)', 'B magnitude','Cumulative Probability']].to_csv())
-        createtxtfile(dataf)                                                                           
-        createjsonfile(jsonlist)
-        createasciifile(jsonlist2)
-        return
+        createtxtfile(dataf,finalgalnamelist, finaldictsorted,graceid,prelim,levelsper,d,ccc)                                                                           
+        createjsonfile(jsonlist,graceid,prelim,levelsper,d,,graceid,prelim,levelsper,d)
+        createasciifile(jsonlist2,graceid,prelim,levelsper,d)
   
           
 #payload = open('S190910d-1-Preliminary.xml', 'rb').read()
