2fde9e55b5e5df706881bc0944bb449c09a7e6ab
Lanasalmon <lana.salmon@ucdconnect.ie>
2019-10-19 12:53:22 +0100

    Top level functions

--- gcn_listener_with_contour_id.py
+++ gcn_listener_with_contour_id.py
@@ -1,49 +1,22 @@
-import astropy.utils.data
-import sys
-sys.path.append('/home/swalsh/anaconda3/lib/python3.6/site-packages')
-import lxml.etree
-import gcn
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-import boto3, botocore
-from email.mime.base import MIMEBase
-from email import encoders
-import smtplib
-import healpy as hp
-import numpy as np
-import lxml.etree
 import sys
 import ligo.skymap.plot
-import astropy_healpix
-from astropy.io import fits
-import numpy.ma as ma
-sys.path.append('/home/swalsh/anaconda3/lib/python3.6/site-packages')
-from db import * 
-from probability import *
-from dataframe import * 
-from contours import * 
-import numpy as np
-import time
-import matplotlib
-import pandas as pd
-from astropy.io import fits
-import psycopg2
+import lxml.etree
+import gcn
 import healpy as hp
-import os
-import json
-import operator
-from numpy.linalg import eig, inv
-import functools
+import numpy as np
+import astropy_healpix
+import astropy.utils.data
 from astropy.coordinates import SkyCoord
 import astropy.units as u
-from astroquery.simbad import Simbad
-import datetime
-from astroquery.vizier import VizierClass
-from astropy.coordinates import SkyCoord
-import astropy.units as u
-from ligo.skymap.io import read_sky_map
+import pandas as pd
 from ligo.skymap.postprocess import crossmatch
-import h5py
+from ligo.skymap.io import read_sky_map
+from probability import calculate_absolute_probability,unique_galaxies,extract_LIGO_probability,makelists,sortbyprob
+from dataframe import create_dataframe
+from contours import contour_plots,join_0_360,split_contours,integrated_probability,hpix_contours,pixels_in_region,checkifinellipsemoc
+from createfiles import createtxt,createjsonfile,createasciifile
+from GLADE import Viziergalaxies
+from skymapio import readskymap 
 
 
 sys.setrecursionlimit(6000000)
@@ -54,6 +27,7 @@ sys.setrecursionlimit(6000000)
     gcn.notice_types.LVC_PRELIMINARY,
     gcn.notice_types.LVC_INITIAL,
     gcn.notice_types.LVC_UPDATE)
+
 
 def process_gcn(payload, root):
 
@@ -77,51 +51,22 @@ def process_gcn(payload, root):
     prelim=params['AlertType']+params['Pkt_Ser_Num']
 
     # Read the HEALPix sky map and the FITS header.
+    skymap, prob, distmu, distsigma, distnorm, npix, nside, distest, diststd=readskymap(params['skymap_fits'])
     
-    hdulist = fits.open(params['skymap_fits'])
-    distest = hdulist[1].header['DISTMEAN']
-    diststd= hdulist[1].header['DISTSTD']
-    
-
-    skymap=read_sky_map(params['skymap_fits'], moc=False,distances=True)
-    
-    prob=skymap[0][0]
-    distmu=skymap[0][1]
-    distsigma=skymap[0][2]
-    distnorm=skymap[0][3]
-    npix = len(prob)
-    nside=hp.npix2nside(npix)
-
+    #create integrated probability map and contours
     csm=integrated_probability(prob)
     contours=hpix_contours(csm,levels=[0.99],nest=False)
 
+    #define percentage localisation regions
     levels=[0.99]
     levelsper=[99]
     
+    #for each percentage localisation region
     for d in range(0,len(contours)):
-
-        datafslong={}
-        datafsshort={}
-        datafslong99={}
-        datafsshort99={}
-        datafslong50={}
-        datafsshort50={}
-        
-        numgals=[]
-        numgals99=[]
-        numgals50=[]
-
-        tablenames=[]
-        tablenames99=[]
-        tablenames50=[]
-
         jsonlist=[]
         jsonlist2=[]
-        jsonlist99=[]
-        jsonlist299=[]
-        jsonlist50=[]
-        jsonlist250=[]
-
+        ccc=[]
+        
         ra_incontourlist=[]
         contourlens=[]
         dec_incontourlist=[]
@@ -142,57 +87,28 @@ def process_gcn(payload, root):
         contourlist1=[]
         contourlist=[]
         contourss=[]
+        
         #separate masked array into separate contours
         split_dec, split_ra = split_contours(contours, levels[d],d)
         
         #retrieve pixels in 50, 90, 99 percent regions
         moc,ipixes=pixels_in_region(levels[d],params['skymap_fits'])
         
-        
         # if the contour is split at 0/360 degrees, rejoin back together
         split_ra2, split_dec2=join_0_360(split_ra, split_dec)
-        
 
         #create a plot of contours and number them
         contour_plots(split_ra2, split_dec2,graceid, prelim, levelsper[d])
        
-
-        
+        #for each separate contour
         for i in range(0,len(split_ra2)):
+            #identify galaxies in circular regions surrounding contour
+            galaxy_ra, galaxy_dec, galaxy_dist, galaxy_Bmag,galaxyname=Viziergalaxies(split_ra2,split_dec2,i,distest,diststd)  
             
-            diff_values_ra = np.diff(split_ra2[i])
-
-            #if the contour crosses 0/360 degrees, splut it up again so we can perform query in Vizier
-            if max(diff_values_ra)>350:
-                
-
-                split_index_ra = np.where(np.abs(diff_values_ra) > 350)[0]
-                split_index_ra += 1
-
-                #split at point where 0 goes to 360
-                split_ra_360, split_dec_360=split_ra_dec(split_ra2, split_dec2, split_index_ra, i)
-                
-                #get centre coordinates and diameter of circle which encloses contour region
-                centreras, centredecs, maxdists =get_centres(split_ra_360, split_dec_360,i)
-                database='new'
-                table='ER4'+graceid+prelim+str(levelsper[d])+'23%i' %i
-                
-                #query vizier using the circle and save in database for that contour
-                galaxy_ra, galaxy_dec, galaxy_dist, galaxy_Bmag, galaxyname=make_database_list(database, table, centreras, centredecs, maxdists, distest, diststd)
-                
-                
-            else: 
-                #if the contour doesn't cross 0/360 degrees, get centre coordinates and diameter of circle
-                centrera,centredec, maxdist=radius(split_ra2[i], split_dec2[i],i)
-                database='new'
-                table='ER4'+graceid+prelim+str(levelsper[d])+'23%i' %i
-                
-                #query vizier using the circle and save in database for that contour
-                galaxy_ra, galaxy_dec, galaxy_dist, galaxy_Bmag,galaxyname=make_database(database, table, centrera, centredec, maxdist, distest, diststd)
-                
             #identify galaxies within localisation region
             ra_incontour, dec_incontour, dist_incontour, Bmag_incontour, name_incontour,contourss=checkifinellipsemoc(moc,nside, ipixes, galaxy_ra, galaxy_dec,galaxy_dist, galaxy_Bmag, galaxyname,i+1)
             contourlens.append(len(ra_incontour))
+            
             # extract probability parameters at galaxy positions
             probs, mudists, distssigma, distsnorm = extract_LIGO_probability(ra_incontour, dec_incontour, nside, distsigma, prob, distnorm, distmu)
             
@@ -206,9 +122,7 @@ def process_gcn(payload, root):
           
         # Calculate probability score
         finalprobs = calculate_absolute_probability(dist_incontourlist1, Bmag_incontourlist1,mudists_incontourlist1, distssigma_incontourlist1,distsnorm_incontourlist1,probs_incontourlist1)
-        database='new'
-        table='ER4'+graceid+prelim+str(levelsper[d])+'probs230'
-
+        
         finalprobss=[]
         for j in range(0,len(finalprobs[0])):
             finalprobss.append(finalprobs[0,j])
@@ -216,50 +130,17 @@ def process_gcn(payload, root):
         finalprobss,ra_incontourlist,dec_incontourlist,finalprobslist,finalgalnamelist, dist_incontourlist,Bmag_incontourlist,contourlist1 = makelists(finalprobss,ra_incontourlist,ra_incontourlist1,dec_incontourlist,dec_incontourlist1,finalprobslist,finalgalnamelist,finalgalname,dist_incontourlist,dist_incontourlist1,Bmag_incontourlist,Bmag_incontourlist1,contourlist1,contourlist)
         
         #sort by descending probability
-        Snumber=np.arange(0,len(finalprobslist),1)
-        numgals.append(len(finalprobslist))
-        fullprobs = dict(zip(Snumber, finalprobslist))
-        finaldictsorted = sorted(fullprobs.items(), key=operator.itemgetter(1), reverse=True)
-        finalprobsorted=sorted(finalprobslist, reverse=True)
-
-        database='new'
-        table='ER4'+graceid+prelim+str(levelsper[d])+'probs230'
-        
-        
-        #create dataframe for jsons
-        cumsumprobs=np.cumsum(finalprobsorted)
+        finaldictsorted, cumsumprobs=sortbyprob(finalprobslist)    
         
         #create dataframe for jsons
         
         dataf=create_dataframe(finaldictsorted, ra_incontourlist, dec_incontourlist, finalgalnamelist, dist_incontourlist, Bmag_incontourlist, contourlist,cumsumprobs)
         jsonlist.append(dataf[['Galaxy name', 'Galaxy probability', 'RA (degrees)', 'Dec (degrees)','Distance (Mpc)', 'B magnitude', 'Contour']].to_json())
         jsonlist2.append(dataf[['Galaxy name', 'Galaxy probability', 'RA (degrees)', 'Dec (degrees)','Distance (Mpc)', 'B magnitude','Contour']].to_csv())
-       
-
-        csv=str('[')
         
-
-        for i in range(0,len(jsonlist)):
-            if i<len(jsonlist)-1:
-                csv= csv+ str(jsonlist[i])+','
-            else:
-                csv = csv + str(jsonlist[i])
-        csv= csv+ ']'
-
-
-        f = open(graceid+prelim+str(levelsper[d])+".json", "w")
-        f.write(csv)      # str() converts to string
-        f.close()
-
-
-        csv2=str('')
-        for i in range(0,len(jsonlist2)):
-
-            csv2= csv2+ str(jsonlist2[i])
-        f = open(graceid+prelim+str(levelsper[d])+".dat", "w")
-        f.write( csv2 )      # str() converts to string
-        f.close()
-
+        createtxt(dataf,finalgalnamelist, finaldictsorted,graceid,prelim,levelsper,d,ccc)                                                                           
+        createjsonfile(jsonlist,graceid,prelim,levelsper,d)
+        createasciifile(jsonlist2,graceid,prelim,levelsper,d)
     
 #payload = open('S190910d-1-Preliminary.xml', 'rb').read()
 #root = lxml.etree.fromstring(payload)
