c3a2a3fa274838a2239ecd4f13dbd8180a5d40a8
Lanasalmon <lana.salmon@ucdconnect.ie>
2019-12-11 12:15:24 +0000

    Add Slum, pdist, ploc to dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -4,7 +4,7 @@ import astropy.units as u
 import pandas as pd
 import json
 
-def create_dataframe(finaldictsorted, ra, dec,name, dist, bmag,contour,cumsumprobs):
+def create_dataframe(finaldictsorted, ra, dec,probs,name, dist, pdist,bmag, Slum,contour,cumsumprobs):
     """
     Create sorted dataframe of galaxies.
     
@@ -39,32 +39,41 @@ def create_dataframe(finaldictsorted, ra, dec,name, dist, bmag,contour,cumsumpro
     finaldist=[]
     finalbmag=[]
     finalcontour=[]
+    finalpdist=[]
+    finalSlum=[]
+    finalorigprob=[]
     cumsum=[]
-    for i in range(0,len(finaldictsorted)):
-        r = 10* u.arcminute
-        finalgalname.append(name[finaldictsorted[i][0]])
-        finalprob.append(finaldictsorted[i][1])
-        finalra.append(ra[finaldictsorted[i][0]])
-        finaldec.append(dec[finaldictsorted[i][0]])
-        finaldist.append(dist[finaldictsorted[i][0]])
-        finalbmag.append(bmag[finaldictsorted[i][0]])
-        finalcontour.append(contour[finaldictsorted[i][0]])
-        cumsum.append(cumsumprobs[i])
-        
+    finalgalname=[name[finaldictsorted[i][0]] for i,x in finaldictsorted]
+    finalprob=[finaldictsorted[i][1] for i,x in finaldictsorted]
+    finalra=[ra[finaldictsorted[i][0]] for i,x in finaldictsorted]
+    finaldec=[dec[finaldictsorted[i][0]] for i,x in finaldictsorted]
+    finaldist=[dist[finaldictsorted[i][0]] for i,x in finaldictsorted]
+    finalbmag=[bmag[finaldictsorted[i][0]] for i,x in finaldictsorted]
+    finalcontour=[contour[finaldictsorted[i][0]] for i,x in finaldictsorted]
+    finalSlum=[Slum[finaldictsorted[i][0]] for i,x in finaldictsorted]
+    finalpdist=[pdist[finaldictsorted[i][0]] for i,x in finaldictsorted]
+    cumsum=[cumsumprobs[i] for i,x in finaldictsorted]
+    finalorigprob=[probs[finaldictsorted[i][0]] for i,x in finaldictsorted]
     
     dataf = pd.DataFrame(
                      {'Galaxy name' : finalgalname,
-                     'Galaxy probability': finalprob,
+                     'Galaxy probability score': finalprob,
                      'RA (degrees)': finalra,
                      'Dec (degrees)': finaldec,
+                     'Location probability score' : finalorigprob,
                      'Galaxy name': finalgalname,
                      'Distance (Mpc)': finaldist,
+                     'Distance probability score': finalpdist,
                      'B magnitude': finalbmag,
+                     'B luminosity probability score': finalSlum,
                      'Contour': finalcontour,
-                     'Cumulative Probability':cumsum
+                     'Cumulative Score':cumsum
                      
                      })
     
-    dataf = dataf[['Galaxy name', 'Galaxy probability', 'RA (degrees)', 'Dec (degrees)','Distance (Mpc)', 'B magnitude', 'Contour', 'Cumulative Probability']]
+    dataf = dataf[['Galaxy name', 'Galaxy probability score', 'RA (degrees)', 'Dec (degrees)','Location probability score','Distance (Mpc)', 'Distance probability score', 'B magnitude', 'B luminosity probability score','Contour', 'Cumulative Score']]
     dataf.to_json('output.json')
     return dataf
+
+
+
