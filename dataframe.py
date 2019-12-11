4344d4c5b70274a873df1fa4b542b8bbf1675bf4
Lanasalmon <lana.salmon@ucdconnect.ie>
2019-12-11 18:49:03 +0000

    List comprehension in dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -31,42 +31,19 @@ def create_dataframe(finaldictsorted, ra, dec,probs,name, dist, pdist,bmag, Slum
     -------
     
     """
-    finalgalname=[]
-    finalprob=[]
-    finalra=[]
-    finaldec=[]
-    timetoset=[]
-    finaldist=[]
-    finalbmag=[]
-    finalcontour=[]
-    finalpdist=[]
-    finalSlum=[]
-    finalorigprob=[]
-    cumsum=[]
-    # finalgalname=[name[finaldictsorted[i][0]] for i,x in finaldictsorted]
-    # finalprob=[finaldictsorted[i][1] for i,x in finaldictsorted]
-    # finalra=[ra[finaldictsorted[i][0]] for i,x in finaldictsorted]
-    # finaldec=[dec[finaldictsorted[i][0]] for i,x in finaldictsorted]
-    # finaldist=[dist[finaldictsorted[i][0]] for i,x in finaldictsorted]
-    # finalbmag=[bmag[finaldictsorted[i][0]] for i,x in finaldictsorted]
-    # finalcontour=[contour[finaldictsorted[i][0]] for i,x in finaldictsorted]
-    # finalSlum=[Slum[finaldictsorted[i][0]] for i,x in finaldictsorted]
-    # finalpdist=[pdist[finaldictsorted[i][0]] for i,x in finaldictsorted]
-    # cumsum=[cumsumprobs[i] for i,x in finaldictsorted]
-    # finalorigprob=[probs[finaldictsorted[i][0]] for i,x in finaldictsorted]
-    for i in range(0,len(finaldictsorted)):
-        r = 10* u.arcminute
-        finalgalname.append(name[finaldictsorted[i][0]])
-        finalprob.append(finaldictsorted[i][1])
-        finalra.append(ra[finaldictsorted[i][0]])
-        finaldec.append(dec[finaldictsorted[i][0]])
-        finaldist.append(dist[finaldictsorted[i][0]])
-        finalbmag.append(bmag[finaldictsorted[i][0]])
-        finalcontour.append(contour[finaldictsorted[i][0]])
-        finalSlum.append(Slum[finaldictsorted[i][0]])
-        finalpdist.append(pdist[finaldictsorted[i][0]])
-        cumsum.append(cumsumprobs[i])
-        finalorigprob.append(probs[finaldictsorted[i][0]])
+
+    finalgalname=[name[finaldictsorted[i][0]] for i,x in enumerate(finaldictsorted)]
+    finalprob=[finaldictsorted[i][1] for i,x in enumerate(finaldictsorted)]
+    finalra=[ra[finaldictsorted[i][0]] for i,x in enumerate(finaldictsorted)]
+    finaldec=[dec[finaldictsorted[i][0]] for i,x in enumerate(finaldictsorted)]
+    finaldist=[dist[finaldictsorted[i][0]] for i,x in enumerate(finaldictsorted)]
+    finalbmag=[bmag[finaldictsorted[i][0]] for i,x in enumerate(finaldictsorted)]
+    finalcontour=[contour[finaldictsorted[i][0]] for i,x in enumerate(finaldictsorted)]
+    finalSlum=[Slum[finaldictsorted[i][0]] for i,x in enumerate(finaldictsorted)]
+    finalpdist=[pdist[finaldictsorted[i][0]] for i,x in enumerate(finaldictsorted)]
+    cumsum=[cumsumprobs[i] for i,x in enumerate(finaldictsorted)]
+    finalorigprob=[probs[finaldictsorted[i][0]] for i,x in enumerate(finaldictsorted)]
+    
         
     
     dataf = pd.DataFrame(
@@ -88,6 +65,8 @@ def create_dataframe(finaldictsorted, ra, dec,probs,name, dist, pdist,bmag, Slum
     dataf = dataf[['Galaxy name', 'Galaxy probability score', 'RA (degrees)', 'Dec (degrees)','Location probability score','Distance (Mpc)', 'Distance probability score', 'B magnitude', 'B luminosity probability score','Contour', 'Cumulative Score']]
     dataf.to_json('output.json')
     return dataf
+
+
 
 
 
