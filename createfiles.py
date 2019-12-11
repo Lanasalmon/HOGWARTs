0e7fe92e58410ef21b385a6e825bf17fe632e811
Lanasalmon <lana.salmon@ucdconnect.ie>
2019-12-11 12:17:30 +0000

    Update createfiles.py for Slum, pdist, ploc

--- createfiles.py
+++ createfiles.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from astropy.coordinates import SkyCoord
 import astropy.units as u
+import time
 def createtxt(dataf, finalgalnamelist, finaldictsorted,graceid,prelim,levelsper,d,ccc):
     c=SkyCoord(ra=dataf['RA (degrees)'].values*u.degree, dec=dataf['Dec (degrees)'].values*u.degree)
 
@@ -8,43 +9,37 @@ def createtxt(dataf, finalgalnamelist, finaldictsorted,graceid,prelim,levelsper,
     newram=[c.ra.hms.m]
     newras=[c.ra.hms.s]
     newdecs=[c.dec]
-
-    newc=[str(int(newrah[0][c]))+ ' ' +str(int(newram[0][c]))+' '+str(newras[0][c].round(2))+ ' '+str(newdecs[0][c]) for c,x in enumerate(newrah)]
-
+    start=time.time()
+    newc=[str(int(newrah[0][c]))+ ' ' +str(int(newram[0][c]))+' '+str(newras[0][c].round(2))+ ' '+str(newdecs[0][c]) for c,x in enumerate(newrah[0])]
+    end=time.time()
+    print('time',end-start)
     newc = [w.replace('d', ' ') for w in newc]
     newc = [w.replace('h', ' ') for w in newc]
     newc = [w.replace('m', ' ') for w in newc]
     newc = [w.replace('s', ' ') for w in newc]
     ccc.append(newc)
+    coords=[x for x in ccc[0]]
+    jtwo=['J2000']*len(ccc[0])
+    name=['COORD'+str(k) for k in range(0,len(ccc[0]))]
+    exclam=['!']*len(ccc[0])
+    galname=[finalgalnamelist[finaldictsorted[d][0]] for d,x in enumerate(ccc[0])]
+    probtxt=[finaldictsorted[d][1] for d,x in enumerate(ccc[0])]
 
-    for i in range(0,len(ccc)):
-        coords=[]
-        name=[]
-        jtwo=[]
-        probtxt=[]
-        galname=[]
-        exclam=[]
-        for k in range(0,len(ccc[i])):
 
-            coords.append(ccc[i][k])
-            jtwo.append('J2000')
-            name.append('COORD0'+str(k))
-            exclam.append('!')
-            galname.append(str(finalgalnamelist[finaldictsorted[k][0]]))
-            probtxt.append(str(finaldictsorted[k][1]))
-        datafc=pd.DataFrame(
-                     {'Galaxy Name' : name,
-                     'Coordinates': coords,
-                     'J2000': jtwo,
-                     '!': exclam,
-                     'Name' :galname,
-                     'Prob': probtxt
-                     })
+        
+    datafc=pd.DataFrame(
+         {'Galaxy Name' : name,
+         'Coordinates': coords,
+         'J2000': jtwo,
+         '!': exclam,
+         'Name' :galname,
+         'Prob': probtxt
+         })
 
-        datafc = datafc[['Galaxy Name', 'Coordinates', 'J2000','!','Name','Prob']]
-        tfile = open(graceid+prelim+str(levelsper[d])+".txt", 'a')
-        tfile.write(datafc.to_string(header=False, index=False))
-        tfile.close()
+    datafc = datafc[['Galaxy Name', 'Coordinates', 'J2000','!','Name','Prob']]
+    tfile = open("/home/swalsh/Desktop/HOGWARTs/static/"+graceid+prelim+str(levelsper[d])+".txt", 'a')
+    tfile.write(datafc.to_string(header=False, index=False))
+    tfile.close()
     return   
 def createjsonfile(jsonlist,graceid,prelim,levelsper,d):
     csv=str('[')
@@ -54,7 +49,7 @@ def createjsonfile(jsonlist,graceid,prelim,levelsper,d):
         else:
             csv = csv + str(jsonlist[i])
     csv= csv+ ']'
-    f = open(graceid+prelim+str(levelsper[d])+".json", "w")
+    f = open("/home/swalsh/Desktop/HOGWARTs/static/"+graceid+prelim+str(levelsper[d])+".json", "w")
     f.write(csv)      # str() converts to string
     f.close()
     return 
@@ -66,7 +61,7 @@ def createasciifile(jsonlist2,graceid,prelim,levelsper,d):
     for i in range(0,len(jsonlist2)):
 
         csv2= csv2+ str(jsonlist2[i])
-    f = open(graceid+prelim+str(levelsper[d])+".dat", "w")
+    f = open("/home/swalsh/Desktop/HOGWARTs/static/"+graceid+prelim+str(levelsper[d])+".dat", "w")
     f.write( csv2 )      # str() converts to string
     f.close()
     return
